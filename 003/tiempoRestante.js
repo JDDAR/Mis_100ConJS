1314e2d4431748b1393b52c7d176bbf30e3e89db
JDDAR <jdanaconar@gmail.com>
2023-09-21 17:38:59 -0500

    ejercicio 003 - 004 General

--- 003/tiempoRestante.js
+++ 003/tiempoRestante.js
@@ -1,37 +1,73 @@
 // Forma 1 
 
-const fechaFinal = new Date('2028-09-26');
-const resultado = document.getElementById('resultado');
+const dateBox = document.querySelector('.input__search');
+const dateBtn = document.querySelector('.btn__search');
 
-const ms_anno = 1000 * 60 * 60 * 24 * 365;
-const ms_mes = 1000 * 60 * 60 * 24 * 30;
-const ms_dia = 1000 * 60 * 60 * 24;
-const ms_hora = 1000 * 60 * 60;
-const ms_minuto = 1000 * 60;
-const ms_segundo = 1000;
 
-setInterval(() => {
+dateBtn.addEventListener("click",( ) =>{ 
+  dateNew(dateBox.value);
+})
+
+
+function dateNew(date) {
+  console.log(date);
+
+  const fechaFinal = new Date(date);
   const hoy = new Date();
-  let resto;
+
+
+  const annosWeb = document.querySelector('.annos');
+  const mesesWeb = document.querySelector('.meses');
+  const diasWeb = document.querySelector('.dias');
+  const horasWeb = document.querySelector('.horas');
+  const minutosWeb =document.querySelector('.minutos ');
+  const segundosWeb = document.querySelector('.segundos');
+
+  const ms_anno = 1000 * 60 * 60 * 24 * 365;
+  const ms_mes = 1000 * 60 * 60 * 24 * 30;
+  const ms_dia = 1000 * 60 * 60 * 24;
+  const ms_hora = 1000 * 60 * 60;
+  const ms_minuto = 1000 * 60;
+  const ms_segundo = 1000;
+
+   if( fechaFinal < hoy ){ 
   
-  const diferenMilisegundos = Math.abs(fechaFinal - hoy);
-  const annos = Math.floor(diferenMilisegundos / ms_anno);
+    resultado.innerText = `la fecha ya paso, intenta con otra fecha`;
+   }else{   
 
-  resto = diferenMilisegundos % ms_anno;
-  const meses = Math.floor(resto / ms_mes);
 
-  resto = diferenMilisegundos % ms_mes;
-  const dias = Math.floor(resto / ms_dia);
+  setInterval(() => {
+      const hoy = new Date(); 
+    let resto;
 
-  resto = diferenMilisegundos % ms_dia;
-  const horas = Math.floor(resto / ms_hora);
+    const diferenMilisegundos = Math.abs(fechaFinal - hoy);
+    const annos = Math.floor(diferenMilisegundos / ms_anno);
 
-  resto = diferenMilisegundos % ms_hora;
-  const minutos = Math.floor(resto / ms_minuto);
+    resto = diferenMilisegundos % ms_anno;
+    const meses = Math.floor(resto / ms_mes);
 
-  resto = diferenMilisegundos % ms_minuto;
-  const segundos = Math.floor(resto / ms_segundo);
+    resto = diferenMilisegundos % ms_mes;
+    const dias = Math.floor(resto / ms_dia);
 
-  resultado.innerText = `${annos} : ${meses} : ${dias} : ${horas} : ${minutos} : ${segundos}`;
-}, 1000)
+    resto = diferenMilisegundos % ms_dia;
+    const horas = Math.floor(resto / ms_hora);
+
+    resto = diferenMilisegundos % ms_hora;
+    const minutos = Math.floor(resto / ms_minuto);
+
+    resto = diferenMilisegundos % ms_minuto;
+    const segundos = Math.floor(resto / ms_segundo);
+
+
+    annosWeb.innerText = `${annos}`;
+    mesesWeb.innerText = `${meses}`;
+    diasWeb.innerText = `${dias}`;
+    horasWeb.innerText= `${horas}`;
+    minutosWeb.innerText = `${minutos}`;
+    segundosWeb.innerText = `${segundos}`;
+  }, 1000)
+
+  }
+
+}
 
